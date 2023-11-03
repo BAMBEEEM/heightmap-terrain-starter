4618ba09033d4b6ce114e1cbe0f373b1c2630d56
Nathan Bean <nhbean@WIN2.CS.KSU.EDU>
2023-11-03 12:05:11 -0500

    Renamed Game1

--- HeightmapTerrainStarter/Program.cs
+++ HeightmapTerrainStarter/Program.cs
@@ -14,7 +14,7 @@ namespace HeightmapTerrainStarter
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ExampleGame())
                 game.Run();
         }
     }
